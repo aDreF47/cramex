1ea86c0ee63b006665660dc3274dff1d072ffc8c
Junior <david2505j@gmail.com>
2025-03-03 01:43:29 -0500

    Agregando query bd

---
+++ database/query/insert.sql
@@ -0,0 +1,38 @@
+/*use App\Models\Producto;
+
+Producto::create([
+    'NombreProducto' => 'Cuaderno de Control',
+    'Descripcion' => 'Cuaderno para el control de asistencias',
+    'Precio' => 15.50,
+    'Stock' => 100
+]);
+
+Producto::create([
+    'NombreProducto' => 'Libro - Matemáticas Avanzadas',
+    'Descripcion' => 'Libro de matemáticas para secundaria con ejercicios prácticos.',
+    'Precio' => 85.00,
+    'Stock' => 30 
+]);
+
+Producto::create([
+    'NombreProducto' => 'Uniforme escolar', 
+    'Descripcion' => 'Camisa, pantalon y casaca escolar.', 
+    'Precio' => 102.50, 
+    'Stock' => 100 
+]);
+
+Producto::create([
+    'NombreProducto' => 'Cordon Policia Escolar', 
+    'Descripcion' => 'Cordón para policia escolar .', 
+    'Precio' => 20.00, 
+    'Stock' => 25 
+]);
+
+Producto::create([
+    'NombreProducto' => 'Libro - Historia del Perú',
+    'Descripcion' => 'Libro de historia con ilustraciones y datos históricos importantes.',
+    'Precio' => 95.00,
+    'Stock' => 40
+]);
+Verificar:D
+Producto::all();*/
